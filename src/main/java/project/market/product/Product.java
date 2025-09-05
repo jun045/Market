62fcbcd1599a90f746604ba8a46cab68e9364cfc
jun <mmy519@naver.com>
2025-09-05 22:35:14 +0900

    fix: 상품-브랜드 ManyToOne, 빌더, update 함수, 소프트 딜리트 추가

--- src/main/java/project/market/product/Product.java
+++ src/main/java/project/market/product/Product.java
@@ -2,13 +2,9 @@ package project.market.product;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import project.market.BaseEntity;
-
-import java.time.LocalDateTime;
+import project.market.Brand.Brand;
 
 @Setter
 @Getter
@@ -18,6 +14,11 @@ public class Product extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //브랜드도 따로 분리해야 한 브랜드 상품 다 보기 가능
+    //private String brandName;
+    @ManyToOne
+    private Brand brand;
 
     @NotNull
     private String productName;
@@ -37,16 +38,60 @@ public class Product extends BaseEntity {
     @NotNull
     private int salePrice; //최종가(세일가)
 
-    //판매상태
+
     @NotNull
-    private productStatus productStatus = project.market.product.productStatus.SALE;
+    @Enumerated(EnumType.STRING)
+    private ProductStatus productStatus; //판매상태
 
-    private String brandName;
-
-    private boolean isDeleted = false;
+    private boolean isDeleted = false; //상품 삭제
 
     //카테고리:상품=1:n
     @ManyToOne
     private Category category;
 
+    //좋아요 개수
+    private int likeCount;
+
+    @Builder
+    public Product(String productName,
+                   String description,
+                   String thumbnail,
+                   String detailImage,
+                   int listPrice,
+                   int salePrice,
+                   ProductStatus productStatus,
+                   boolean isDeleted,
+                   Category category,
+                   int likeCount) {
+        this.productName = productName;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.detailImage = detailImage;
+        this.listPrice = listPrice;
+        this.salePrice = salePrice;
+        this.productStatus = productStatus;
+        this.isDeleted = isDeleted;
+        this.category = category;
+        this.likeCount = likeCount;
+    }
+
+    //할인된 가격 계산
+
+    //정보 수정
+    public void update (String productName,
+                   String description,
+                   String thumbnail,
+                   String detailImage,
+                   int listPrice) {
+        this.productName = productName;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.detailImage = detailImage;
+        this.listPrice = listPrice;
+    }
+
+    //소프트 딜리트 : 탈퇴시 true
+    public void deletedProduct(){
+        this.isDeleted = true;
+    }
 }

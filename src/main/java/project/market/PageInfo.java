a509b2b90e1a67fd5456c5dd0d7e6d5bf65b7778
HYOJIN <hjhwang08@naver.com>
2026-01-06 11:44:47 +0900

    feat: 결제 내역 조회 페이지네이션

---
+++ src/main/java/project/market/PageInfo.java
@@ -0,0 +1,7 @@
+package project.market;
+
+public record PageInfo (int pageNumber,
+                        int size,
+                        long totalElement,
+                        int totalPage){
+}

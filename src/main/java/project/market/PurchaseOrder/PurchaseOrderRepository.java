893856a7ab71c51c86c5c2356ac4205b73bb449d
jun <mmy519@naver.com>
2026-03-17 03:27:31 +0900

    perf : 전체조회(관리자,사용자) JPQL fetch join로 변경 - 관리자 : 모든 주문 다 보임 - 사용자 : 삭제된 주문은 조회 불가

--- src/main/java/project/market/PurchaseOrder/PurchaseOrderRepository.java
+++ src/main/java/project/market/PurchaseOrder/PurchaseOrderRepository.java
@@ -1,6 +1,8 @@
 package project.market.PurchaseOrder;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.market.PurchaseOrder.entity.PurchaseOrder;
 
 import java.util.List;
@@ -10,4 +12,21 @@ public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder,Lon
     List<PurchaseOrder> findByMemberId(Long memberId);
     Optional<PurchaseOrder> findByMerchantUid(String merchantUid);
 
+    //관리자용 - 삭제된 주문도 조회 가능
+    @Query("SELECT DISTINCT o FROM PurchaseOrder o " +
+            "LEFT JOIN FETCH o.member " +
+            "LEFT JOIN FETCH o.orderItems oi " +
+            "LEFT JOIN FETCH oi.productVariant pv " +
+            "LEFT JOIN FETCH pv.product ")
+    List<PurchaseOrder> findAllWithDetails();
+
+    //사용자용 - 삭제된 주문 조회 불가(isDeleted=false 조건)
+    @Query("SELECT DISTINCT o FROM PurchaseOrder o " +
+            "LEFT JOIN FETCH o.member " +
+            "LEFT JOIN FETCH o.orderItems oi " +
+            "LEFT JOIN FETCH oi.productVariant pv " +
+            "LEFT JOIN FETCH pv.product " +
+            "WHERE o.member.id = :memberId " +
+            "AND o.isDeleted = false")
+    List<PurchaseOrder> findAllWithDetailsByMemberId(@Param("memberId") Long memberId);
 }
